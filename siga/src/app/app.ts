98348f5e0a257280758f9aebc73279d13cfe1c57
AMTS18 <64600055+AlexSantos2002@users.noreply.github.com>
2026-03-13 17:19:30 +0000

    navbar v1
    Primeira versão da componente da navbar

--- siga/src/app/app.ts
+++ siga/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from './components/footer/footer';
+import { Navbar } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
