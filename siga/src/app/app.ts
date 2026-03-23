4b69b0b5addba3fab6eabf9ec6bc2ede016b63b8
AMTS18 <64600055+AlexSantos2002@users.noreply.github.com>
2026-03-23 05:50:16 +0000

    navbar

--- siga/src/app/app.ts
+++ siga/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Footer } from './components/footer/footer';
+import { Navbar } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Footer],
+  imports: [Footer, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
