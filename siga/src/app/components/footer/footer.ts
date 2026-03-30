306ed1077101a44a4c21be9d4a7c925c500c45f0
AMTS18 <64600055+AlexSantos2002@users.noreply.github.com>
2026-03-30 23:38:45 +0100

    Update estilo footer e about page
    Foi retirado informações repetitivas da footbar que estavam noutras pages das paginas public.
    
    Mudança na page about para colocar uma imagem em vez do quadrado com background azul anterior

--- siga/src/app/components/footer/footer.ts
+++ siga/src/app/components/footer/footer.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
+  imports: [RouterModule],
   templateUrl: './footer.html',
   styleUrl: './footer.css'
 })
-export class Footer {}
+export class Footer {
+
+}
