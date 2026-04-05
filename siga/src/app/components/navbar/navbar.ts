3a3e3615cba445a1968fc1e9d995395efc888cd1
AMTS18 <64600055+AlexSantos2002@users.noreply.github.com>
2026-04-05 06:33:51 +0100

    Botão para troca de lingua na navbar

--- siga/src/app/components/navbar/navbar.ts
+++ siga/src/app/components/navbar/navbar.ts
@@ -1,17 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 /**
  * @description
  * Componente responsável pela barra de navegação
- * 
- * Este componente é utilizado nas páginas públicas e permite a navegação entre diferentes secções do site através do Angular Router
  */
 @Component({
   selector: 'app-navbar',
   standalone: true,
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
-  imports: [RouterModule]
+  imports: [RouterModule, CommonModule]
 })
-export class Navbar {}
+export class Navbar {
+
+  /**
+   * @description
+   * Controla a visibilidade do menu de seleção de idioma
+   */
+  showLangMenu: boolean = false;
+
+  /**
+   * @description
+   * Alterna a visibilidade do menu de idiomas
+   *
+   * @returns {void} Não retorna valor
+   */
+  toggleLangMenu(): void {
+    this.showLangMenu = !this.showLangMenu;
+  }
+
+  /**
+   * @description
+   * Define o idioma selecionado pelo utilizador.
+   *
+   * @param lang Código do idioma 
+   * @returns {void} Não retorna valor
+   */
+  changeLang(lang: string): void {
+    console.log('Idioma selecionado:', lang);
+    localStorage.setItem('lang', lang);
+    this.showLangMenu = false;
+  }
+}
