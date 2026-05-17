fa19bbe6d0401b9c96a972e2f9b0dd1614cf25a6
AMTS18 <64600055+AlexSantos2002@users.noreply.github.com>
2026-05-17 04:17:37 +0100

    Sidemenu 1.0
    Componente side menu, com botões para as opções que terá no website

--- siga/src/app/pages/private/private-layout.ts
+++ siga/src/app/pages/private/private-layout.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { NavbarPrivate } from '../../components/navbar-private/navbar-private';
+import { SideMenu } from '../../components/side-menu/side-menu';
 
 @Component({
   selector: 'app-private-layout',
   standalone: true,
-  imports: [RouterOutlet, NavbarPrivate],
+  imports: [RouterOutlet, NavbarPrivate, SideMenu],
   templateUrl: './private-layout.html',
   styleUrl: './private-layout.css',
 })
